ea72e9e8c17fff61f060ba8ddfbe56b3f6592080
Buse Yılmaz <bsylm@outlook.com>
2024-11-12 17:46:26 +0300

    Proje dosyası ekle.

---
+++ Basic Examples/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Basic_Examples
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Not();
+
+            Console.Read();
+        }
+
+        static void Not()
+        {
+            try
+            {
+                int toplam = 0;
+                Console.Write("Lütfen toplamda kaç tane dersiniz varsa sayısını giriniz:");
+
+                int ders = Convert.ToInt32(Console.ReadLine());
+
+                for (int i = 0; i < ders; i++)
+                {
+                    Console.Write((i + 1) + ". dersi giriniz:");
+                    int notlar = Convert.ToInt32(Console.ReadLine());
+                    toplam += notlar;
+                }
+
+                int ortalama = toplam / ders;
+
+                string sonuc=(ortalama > 60) ? "Sınıfı Geçtin" : "Sınıfta Kaldın";
+
+                Console.Write("Sonucunuz:" + sonuc);
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("HATA");
+            }
+
+            
+        }
+    }
+}

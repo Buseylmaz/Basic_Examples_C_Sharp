0983eed10cf243d1483a1e5a7422ff39018c54fc
Buse Yılmaz <bsylm@outlook.com>
2024-11-13 11:48:25 +0300

    Sıralama
    Girilen n kadar sayıyı "büyükten küçüge" sıralayan program

--- Sayıları Büyükten Küçüğe Sıralama/Program.cs
+++ Sayıları Büyükten Küçüğe Sıralama/Program.cs
@@ -10,6 +10,36 @@ namespace SayilariBuyuktenKucugeSiralama
     {
         static void Main(string[] args)
         {
+            Sayilar();
+
+            Console.ReadLine();
+        }
+
+        static void Sayilar()
+        {
+            Console.Write("Kaç tane sayı girilecek?:");
+            int kac = Convert.ToInt32(Console.ReadLine());
+
+            int[] sayi = new int[kac];
+
+            for (int i = 0; i < kac; i++)
+            {
+                Console.Write("Sayı giriniz:");
+                int s = Convert.ToInt32(Console.ReadLine());
+                sayi[i] = s;
+            }
+
+            Array.Sort(sayi);
+
+            Array.Reverse(sayi);
+
+            foreach (var sa in sayi)
+            {
+                Console.Write(sa+"-");
+            }
+            
+            
+
         }
     }
 }
